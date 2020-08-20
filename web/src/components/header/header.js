a9a0ff6142b5e21f15139066059c146f373e9f28
Caleb Hagner <caleb.hagner@pm.me>
2020-08-20 09:56:07 -0400

    💄 Update Styling

--- web/src/components/header/header.js
+++ web/src/components/header/header.js
@@ -5,29 +5,29 @@ import { cn } from '../../lib/helpers';
 
 import styles from './header.module.css';
 
-const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
-  <div className={styles.root}>
-    <div className={styles.wrapper}>
-      <div className={styles.branding}>
-        <Link to='/'>{siteTitle}</Link>
+export default function Header({ onHideNav, onShowNav, showNav, siteTitle }) {
+  return (
+    <div className={styles.root}>
+      <div className={styles.wrapper}>
+        <div className={styles.branding}>
+          <Link to='/'>{siteTitle}</Link>
+        </div>
+
+        <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
+          <Icon symbol='hamburger' />
+        </button>
+
+        <nav className={cn(styles.nav, showNav && styles.showNav)}>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/blog/'>Blog</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-
-      <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
-        <Icon symbol='hamburger' />
-      </button>
-
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/blog/'>Blog</Link>
-          </li>
-        </ul>
-      </nav>
     </div>
-  </div>
-);
-
-export default Header;
+  );
+}
--- web/src/components/layout/layout.js
+++ web/src/components/layout/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../header/header';
 import Headroom from 'react-headroom';
+import EmailIcon from '@material-ui/icons/Email';
 
 import '../../styles/layout.css';
 import styles from './layout.module.css';
@@ -13,12 +14,7 @@ const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
     <div className={styles.content}>{children}</div>
     <footer className={styles.footer}>
       <div className={styles.footerWrapper}>
-        <div className={styles.siteInfo}>
-          &copy; {new Date().getFullYear()} Caleb Hagner, Built with{' '}
-          <a href='https://www.sanity.io'>Sanity</a> &amp;
-          {` `}
-          <a href='https://www.gatsbyjs.org'>Gatsby</a>
-        </div>
+        <div className={styles.siteInfo}>&copy; {new Date().getFullYear()} Caleb Hagner</div>
       </div>
     </footer>
   </>
