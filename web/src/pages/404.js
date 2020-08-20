a038e27e00c337974ab69dd289c4c9cc78abb322
Caleb Hagner <caleb.hagner@pm.me>
2020-08-20 08:56:38 -0400

    Modify 404 page

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-import Layout from '../components/layout/layout'
-import SEO from '../components/seo/seo'
+import Layout from '../containers/layout';
+import SEO from '../components/seo/seo';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title='404: Not found' />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div style={{ textAlign: 'center' }}>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist...</p>
+    </div>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
